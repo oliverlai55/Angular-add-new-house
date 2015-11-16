f418930ba7627c860d3f99f6bc17cc0ed97900a6
oliverlai55 <oliverlai55@gmail.com>
2015-11-16 18:05:40 -0500

    Add New House button works

--- controller.js
+++ controller.js
@@ -1,23 +1,61 @@
 
-    angular.module('houseList', []).controller('housesListCtrl', housesListCtrl);
+var myApp = angular.module('houseList', []);
+myApp.controller('housesListCtrl', housesListCtrl);
+myApp.controller('editController', editController);
 
-    function housesListCtrl($scope) {
+function housesListCtrl($scope) {
 
-    	$scope.houses = houses;
+   $scope.houses = houses;
+   $scope.view = 'list'
 
-    	var index = 0;
+   var index = 0;
 
-    	
+   $scope.addHouse = function(){
+    $scope.houses.push(houses[index++])
+}
 
-        $scope.addHouse = function(){
-    		$scope.houses.push(houses[index++])
-    	}
+$scope.enableAdd = function(){
+    index < houses.length;
+}
+}
 
-    	$scope.enableAdd = function(){
-    		index < houses.length;
-    	}
+function editController($scope){
+    $scope.houses = houses;
+    $scope.view = 'list'
+    var viewIndex = -1
 
+    $scope.changeView = function(view){
+        $scope.view = view;
     }
 
 
- 
+    $scope.addNewHouse = function(){
+        $scope.houses.push(new Houses($scope.name, $scope.photo, $scope.address, $scope.forSale, $scope.price, $scope.zestimate, $scope.deal, $scope.mortgage))
+        $scope.changeView('list')
+        $scope.name = '';
+        $scope.photo = '';
+        $scope.address = '';
+        $scope.forSale = '';
+        $scope.price = '';
+        $scope.zestimate = '';
+        $scope.deal = '';
+        $scope.mortgage = '';
+        console.log(houses);
+
+    }
+
+    $scope.editHouse = function(index){
+        $scope.changeView('edit');
+        $scope.name = $scope.houses[index].name
+        viewIndex = index;
+    }
+
+    $scope.confirmEdit = function(){
+        $scope.houses[index].name = $scope.name
+        $scope.name = ''
+        $scope.changeView('list')
+    }
+}
+
+
+
